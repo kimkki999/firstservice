aae7e6dcf51baf28f5c73d90aefbce1f82e1303b
kimkki999 <kimkki999@naver.com>
2021-03-12 11:59:56 +0900

    modify for annotation

--- src/main/java/com/example/kki/web/IndexController.java
+++ src/main/java/com/example/kki/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.kki.web;
 
+import com.example.kki.config.auth.LoginUser;
 import com.example.kki.config.auth.dto.SessionUser;
 import com.example.kki.service.posts.PostsService;
 import com.example.kki.web.dto.PostsResponseDto;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
